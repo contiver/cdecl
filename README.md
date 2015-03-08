18cfa6fb829b13e359120cd27506a152ab28ffe3
contiver <cristianontivero@gmail.com>
2015-03-08 13:00:19 -0300

    added readme

---
+++ README.md
@@ -0,0 +1,9 @@
+cdecl - C declarations translator
+=================================
+
+Build
+-----
+By default gcc is used to compile and dynamically link the object file.
+Ideally, statically link with the musl library by changing the options on the
+config.mk file
+To build, just enter `make`
--- config.mk
+++ config.mk
@@ -1,6 +1,12 @@
-CC = musl-gcc
-LDFLAGS = -static -s
-CFLAGS = -static -std=c99 -pedantic -Ofast \
+# Uncomment to statically link with musl
+#CC      = musl-gcc
+#LDFLAGS = -static -s
+#CFLAGS  = -std=c99 -pedantic -Ofast \
+
+CC      = gcc
+LDFLAGS = -s
+CFLAGS  = -std=c99 -pedantic -O0 \
+
 #CFLAGS = -static -std=c99 -pedantic -Wall -Wextra -Wunused-macros \
 	-Wno-missing-braces -Wno-missing-field-initializers -Wformat=2 \
 	-Wswitch-default -Wswitch-enum -Wcast-align -Wpointer-arith \
